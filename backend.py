fbd7de7d3fbcd3e7bc8e8a5249dd8d1af54a75ed
Shreyash <write.shrey@gmail.com>
2024-12-21 21:37:25 +0000

    Connect to MongoDB

--- backend.py
+++ backend.py
@@ -1,6 +1,37 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from fastapi import FastAPI
+from motor import motor_asyncio
+from starlette.middleware.cors import CORSMiddleware
+
+from config import BaseConfig
+
+settings = BaseConfig()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.client = motor_asyncio.AsyncIOMotorClient(settings.DB_URL)
+    app.db = app.client[settings.DB_NAME]
+
+    try:
+        app.client.admin.command("ping")
+        print("Success in pinging your db")
+    except Exception as e:
+        print(e)
+
+    yield
+    app.client.close()
+
+
+app = FastAPI(lifespan=lifespan)
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"]
+)
 
 @app.get("/")
 async def root():
